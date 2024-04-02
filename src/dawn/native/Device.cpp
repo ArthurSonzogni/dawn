0470586298b55d957bcfade4137b4b33a4b1ace5
Jiang <zhaoming.jiang@intel.com>
2024-04-02 02:35:08 +0000

    [M123] Dawn D3D12: Forbid SM6.6 by default on Intel Gen12 devices
    This CL distinguishes highest supported shader model version and actual
    applied shader model version for D3D12 backend, add a adapter toggle
    D3D12DontUseShaderModel66OrHigher that enforce using SM6.5 or lower for
    an adapter, and by default enable this toggle for Intel Gen12 devices to
    workaround issue tint:2189.
    
    Bug: dawn:2470, tint:2189
    Change-Id: I33c9ab65827836bd139b689149d3adb2c2d1ff9f
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/178983
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Reviewed-by: Jiawei Shao <jiawei.shao@intel.com>
    (cherry picked from commit 294864f9e0b41673c8a892e7ac131a07d9409bda)
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/181143
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>

--- src/dawn/native/Device.cpp
+++ src/dawn/native/Device.cpp
@@ -2100,6 +2100,10 @@ bool DeviceBase::IsToggleEnabled(Toggle toggle) const {
     return mToggles.IsEnabled(toggle);
 }
 
+const TogglesState& DeviceBase::GetTogglesState() const {
+    return mToggles;
+}
+
 void DeviceBase::ForceSetToggleForTesting(Toggle toggle, bool isEnabled) {
     mToggles.ForceSet(toggle, isEnabled);
 }
