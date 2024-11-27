e5ef562c1a22d5be201f2970b085bb4d24a6770c
Antonio Maiorano <amaiorano@google.com>
2024-11-27 12:01:25 +0000

    [M126-LTS] IR: Fix robustness transform on textureLoad of sampled and depth textures
    For sampled and depth textures, which contain a 'level' argument, the
    robustness transform is supposed to clamp 'coords' using the dimensions
    at the clamped level, but it was looking up dimensions at level 0
    instead.
    
    Bug: 42250751
    Bug: 42251045
    Bug: 378541479
    Change-Id: I0e7fd6148417b248a9b584ae19818e9027306b63
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/214514
    Reviewed-by: James Price <jrprice@google.com>
    Commit-Queue: dan sinclair <dsinclair@chromium.org>
    Commit-Queue: James Price <jrprice@google.com>
    Auto-Submit: Antonio Maiorano <amaiorano@google.com>
    Reviewed-by: dan sinclair <dsinclair@chromium.org>
    (cherry picked from commit 4eec0a86eb532b7a4111be6bc604bbe45f2b77fe)
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/215455
    Reviewed-by: Antonio Maiorano <amaiorano@google.com>

--- src/tint/lang/core/ir/transform/robustness.cc
+++ src/tint/lang/core/ir/transform/robustness.cc
@@ -321,7 +321,6 @@ struct State {
                 break;
             }
             case core::BuiltinFn::kTextureLoad: {
-                clamp_coords(1u);
                 uint32_t next_arg = 2u;
                 if (type::IsTextureArray(texture->dim())) {
                     clamp_array_index(next_arg++);
@@ -329,6 +328,7 @@ struct State {
                 if (texture->IsAnyOf<type::SampledTexture, type::DepthTexture>()) {
                     clamp_level(next_arg++);
                 }
+                clamp_coords(1u);  // Must run after clamp_level
                 break;
             }
             case core::BuiltinFn::kTextureStore: {
