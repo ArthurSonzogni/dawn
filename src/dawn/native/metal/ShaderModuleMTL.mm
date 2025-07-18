46b4670bc67cb4f6d34f6ce6a46ba7e1d6059abf
Sunny Sachanandani <sunnyps@chromium.org>
2025-07-18 16:27:38 -0700

    [m139] [graphite] [metal] Emit MSL in error message on failed pipeline creation
    We currently emit MSL when shader module compilation fails, but not when
    the subsequent render pipeline creation fails. Keep the MSL around a bit
    longer so that we can emit it on render pipeline creation failure.
    
    Bug: chromium:422989722, chromium:431806025, chromium:412570905
    Change-Id: I6a6a6964a4282c50d06c6f69e8df23053f31380e
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/253574
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    (cherry picked from commit 4b97c56324e5b31394618a40b9946c73ac24abcf)
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/253754
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>

--- src/dawn/native/metal/ShaderModuleMTL.mm
+++ src/dawn/native/metal/ShaderModuleMTL.mm
@@ -218,7 +218,6 @@ ResultOrError<CacheResult<MslCompilation>> TranslateToMSL(
     const ProgrammableStage& programmableStage,
     SingleShaderStage stage,
     const PipelineLayout* layout,
-    ShaderModule::MetalFunctionData* out,
     uint32_t sampleMask,
     const RenderPipeline* renderPipeline,
     const BindingInfoArray& moduleBindingInfo) {
@@ -429,7 +428,7 @@ MaybeError ShaderModule::CreateFunction(SingleShaderStage stage,
 
     CacheResult<MslCompilation> mslCompilation;
     DAWN_TRY_ASSIGN(mslCompilation,
-                    TranslateToMSL(GetDevice(), programmableStage, stage, layout, out, sampleMask,
+                    TranslateToMSL(GetDevice(), programmableStage, stage, layout, sampleMask,
                                    renderPipeline, GetEntryPoint(entryPointName).bindings));
 
     out->needsStorageBufferLength = mslCompilation->needsStorageBufferLength;
@@ -461,9 +460,13 @@ MaybeError ShaderModule::CreateFunction(SingleShaderStage stage,
     }
 
     if (error != nullptr) {
+        // clang-format formats the `mslCompilation->msl` below oddly as `mslCompilation -> msl`.
+        // clang-format off
         DAWN_INVALID_IF(error.code != MTLLibraryErrorCompileWarning,
-                        "Unable to create library object: %s.",
-                        [error.localizedDescription UTF8String]);
+                        "ShaderModuleMTL: Unable to create library object: %s from "
+                        "produced MSL shader below:\n\n%s",
+                        [error.localizedDescription UTF8String], mslCompilation->msl);
+        // clang-format on
     }
     DAWN_ASSERT(library != nil);
     timer.RecordMicroseconds("Metal.newLibraryWithSource.CacheMiss");
@@ -482,7 +485,6 @@ MaybeError ShaderModule::CreateFunction(SingleShaderStage stage,
                 availableFunctions += "\n - \"";
                 availableFunctions += [fn UTF8String];
             }
-
             return DAWN_FORMAT_INTERNAL_ERROR(
                 "ShaderModuleMTL: failed to get the MTLFunction \'%s\' from produced MSL "
                 "shader below:\n\n%s\n\nAvailable functions are:%s",
@@ -499,6 +501,9 @@ MaybeError ShaderModule::CreateFunction(SingleShaderStage stage,
         GetEntryPoint(entryPointName).usedVertexInputs.any()) {
         out->needsStorageBufferLength = true;
     }
+
+    // For emitting MSL in error message if render pipeline creation fails.
+    out->msl = std::move(mslCompilation->msl);
 
     return {};
 }
