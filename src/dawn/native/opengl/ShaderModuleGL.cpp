766364b1c3bdd70997fa6066ac22737758413917
Stephen White <senorblanco@chromium.org>
2026-06-12 06:52:44 -0700

    [M144-LTS] GL: add missing stage visibility check
    In GenerateArrayLengthFromUniformData(), skip bindings that are
    not visible to the current stage.
    
    Bug: 517031505
    
    Change-Id: Ida25ca9233a6880461f67a177e9d2257c1727094
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/313155
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Stephen White <senorblanco@chromium.org>
    (cherry picked from commit 73591422f504902c597a67beaeb8e22101f030bd)
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/314675
    Reviewed-by: Mohamed Omar <mohamedaomar@google.com>
    Owners-Override: Mohamed Omar <mohamedaomar@google.com>

--- src/dawn/native/opengl/ShaderModuleGL.cpp
+++ src/dawn/native/opengl/ShaderModuleGL.cpp
@@ -264,6 +264,7 @@ void GenerateTextureBuiltinFromUniformData(
 bool GenerateArrayLengthFromuniformData(
     const BindingInfoArray& moduleBindingInfo,
     const PipelineLayout* layout,
+    SingleShaderStage stage,
     tint::glsl::writer::ArrayLengthFromUniformOptions& options) {
     const PipelineLayout::BindingIndexInfo& indexInfo = layout->GetBindingIndexInfo();
 
@@ -272,6 +273,11 @@ bool GenerateArrayLengthFromuniformData(
 
         for (BindingIndex binding : bgl->GetBufferIndices()) {
             const BindingInfo& bindingInfo = bgl->GetBindingInfo(binding);
+
+            // Skip bindings that aren't visible to this stage.
+            if (!(bindingInfo.visibility & StageBit(stage))) {
+                continue;
+            }
 
             switch (std::get<BufferBindingInfo>(bindingInfo.bindingLayout).type) {
                 case wgpu::BufferBindingType::Storage:
@@ -408,7 +414,7 @@ ResultOrError<GLuint> ShaderModule::CompileShader(
 
     if (GetDevice()->IsToggleEnabled(Toggle::GLUseArrayLengthFromUniform)) {
         *needsSSBOLengthUniformBuffer = GenerateArrayLengthFromuniformData(
-            moduleBindingInfo, layout, req.tintOptions.array_length_from_uniform);
+            moduleBindingInfo, layout, stage, req.tintOptions.array_length_from_uniform);
         if (*needsSSBOLengthUniformBuffer) {
             req.tintOptions.use_array_length_from_uniform = true;
             req.tintOptions.array_length_from_uniform.ubo_binding = {
