571752c9cb3ed36a3194854374984427794c4dda
Kai Ninomiya <kainino@chromium.org>
2026-08-14 17:07:38 -0700

    [M152] [dawn][vulkan] Disallow NPOT mipmapped depth/stencil textures on ImgTec
    Original change's description:
    > [dawn][vulkan] Disallow NPOT mipmapped depth/stencil textures on ImgTec
    >
    > Can't think of a way to work around this, so just disable it.
    >
    > Fixes: 540087398
    > Cq-Include-Trybots: luci.chromium.try:android-dawn-arm64-p10-rel
    > Change-Id: I22ce9c18564d338c9ee15e18cfe4598bc34cda95
    > Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/328455
    > Reviewed-by: Brandon Jones <bajones@chromium.org>
    > Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    
    (cherry picked from commit 178fb7048ad322e4ff797210358ed05254289e0c)
    
    Bug: 541496703
    Change-Id: I22ce9c18564d338c9ee15e18cfe4598bc34cda95
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/332715
    Reviewed-by: Loko Kung <lokokung@google.com>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>

--- src/dawn/native/Toggles.cpp
+++ src/dawn/native/Toggles.cpp
@@ -815,6 +815,11 @@ static constexpr ToggleEnumAndInfoList kToggleNameAndInfoList = {{
      {"use_spirv_reconvergence_mode",
       "Use SPIR-V reconvergence execution mode if supported by the device.",
       "https://crbug.com/379673383", ToggleStage::Device}},
+    {Toggle::VulkanDisallowNPOTDepthStencilMipmaps,
+     {"vulkan_disallow_npot_depth_stencil_mipmaps",
+      "Reject NPOT depth/stencil textures with mipLevelCount > 1. Workaround for mip level "
+      "miscomputation in PowerVR proprietary driver.",
+      "https://crbug.com/540087398", ToggleStage::Device}},
     {Toggle::WaitIsThreadSafe,
      {"wait_is_thread_safe",
       "WaitFor* functions are thread-safe and can be called without the device-lock if implicit "
