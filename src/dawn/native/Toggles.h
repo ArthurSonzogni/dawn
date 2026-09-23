74de81c66831439913bf1ac97918230fb16e3760
Shrek Shao <shrekshao@google.com>
2026-09-23 10:57:45 -0700

    [M155] [gl] Add compute blit for Sint and Uint texture formats
    Original change's description:
    > [gl] Add compute blit for Sint and Uint texture formats
    >
    > This CL:
    > - Adds toggles UseBlitForUintTextureToBufferCopy and
    >   UseBlitForSintTextureToBufferCopy, enabled by default on OpenGLES.
    > - Implements compute shader blits for integer (Uint and Sint) textures
    >   in BlitTextureToBuffer.
    > - Routes integer formats to compute shader blits on OpenGLES except
    >   RGBA32Uint and RGBA32Sint, which are the only integer formats
    >   guaranteed by the OpenGL ES 3.1 spec to be supported by glReadPixels
    >   with matching data layout.
    > - Bypasses compute shader blit for integer cube textures in compat mode.
    >   In WGSL, texture_cube<u32> and texture_cube<i32> do not support
    >   textureSampleLevel or textureLoad, and textureGather only operates on
    >   mip level 0 for a single component. Therefore, integer cube textures
    >   fall back to glReadPixels via FramebufferTexture2D for each cube face.
    > - Expands CopyTests_T2B and CopyTests_T2B_Compat format parameters to
    >   cover integer formats.
    >
    > Bug: 556959073, 562077184
    > Change-Id: Ica78ec08b6702ea386c7d95e36b5f2fd43a1b02b
    > TAG=agy
    > CONV=8a0c15f2-fbb5-4cb0-bd81-240982407cd0
    > Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/340755
    > Reviewed-by: Geoff Lang <geofflang@chromium.org>
    > Commit-Queue: Shrek Shao <shrekshao@google.com>
    
    (cherry picked from commit 4d90aa23afd490070ac38b6244aa875418a201b0)
    
    Bug: 564785933,556959073,562077184
    Change-Id: Ica78ec08b6702ea386c7d95e36b5f2fd43a1b02b
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/344275
    Commit-Queue: Shrek Shao <shrekshao@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>

--- src/dawn/native/Toggles.h
+++ src/dawn/native/Toggles.h
@@ -127,6 +127,8 @@ enum class Toggle {
     UseBlitForB2T,
     UseBlitForNonRGBAUnormTextureToBufferCopy,
     UseBlitForNonRGBAFloatTextureToBufferCopy,
+    UseBlitForUintTextureToBufferCopy,
+    UseBlitForSintTextureToBufferCopy,
     VulkanSplitBufferTextureCopyForArrayLayers,
     GLUseArrayLengthFromUniform,
     D3D11DisableCPUUploadBuffers,
